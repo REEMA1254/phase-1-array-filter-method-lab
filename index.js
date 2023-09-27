9a6b5b309790df1bb5af7b61c238b7225911e179
maryam <Reemaali9048@gmail.com>
2023-09-28 02:08:03 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter((driver) =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name.toLowerCase() === name.toLowerCase());
+  }
+
+  const drivers = [
+    { name: "John", hometown: "New York" },
+    { name: "Jane", hometown: "Los Angeles" },
+    { name: "Steve", hometown: "Chicago" },
+    { name: "Mike", hometown: "Miami" },
+  ];
+  
+  console.log(findMatching(drivers, "john")); // Output: [{ name: "John", hometown: "New York" }]
+  console.log(fuzzyMatch(drivers, "jane")); // Output: [{ name: "Jane", hometown: "Los Angeles" }]
+  console.log(matchName(drivers, "Mike")); // Output: [{ name: "Mike", hometown: "Miami" }]
+
+
